5daeeb843a2d503b7cb87af5558b92e05dad20b0
Davoleo <davoleo2001@gmail.com>
2020-08-11 11:26:50 +0200

    Add MongoDB to the db services script

--- start_android_avd.bat
+++ start_android_avd.bat
@@ -5,6 +5,7 @@ cd /d D:\Programmi\Android\emulator
 
 echo Requesting Admin Privileges...
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+cls
 
 @title AVD Launch Script
 echo.
--- start_db_service.bat
+++ start_db_service.bat
@@ -3,8 +3,22 @@
 echo Requesting Admin Privileges...
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 
+echo Choose one of the Database Services:
+echo 1) MySQL
+echo 2) MongoDB
+
+SET /p SERV="Your choice: "
+
+if "%SERV%"=="1" then GOTO :1 
+else GOTO :2
+
+:1
 echo Starting MySQL Database Service...
 sc start MySQL-Database-Server
+
+:2
+echo Starting MongoDB Database Service...
+net start MongoDB
 
 echo Complete!
 PAUSE
